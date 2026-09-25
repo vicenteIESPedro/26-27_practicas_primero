64a44191ed1254c995435252679c736397e3481c
atorcas14 <atorcas@g.educaand.es>
2026-09-25 14:42:48 +0200

    Cambios

--- web/aplicacion/pruebas/index.php
+++ web/aplicacion/pruebas/index.php
@@ -1,29 +1,22 @@
 <?php
-include_once(dirname(__FILE__) . "/cabecera.php");
+include_once(dirname(__FILE__) . "/../../cabecera.php");
 //controlador
-
-$usuario=getenv("MYSQL_USER");
-
 //dibuja la plantilla de la vista
 inicioCabecera("APLICACION PRIMER TRIMESTRE");
 cabecera();
 finCabecera();
 inicioCuerpo("2DAW APLICACION");
-cuerpo();  //llamo a la vista
+cuerpo(); //llamo a la vista
 finCuerpo();
 // **********************************************************
 
 //vista
-function cabecera() 
-{}
-
+function cabecera() {}
 //vista
 function cuerpo()
 {
 ?>
     <br><br>
-    Hola, estás en Index.php
-    adios
-    <a href="./aplicacion/pruebas/index.php">Acceso a pruebas</a>
+    Elemento de pruebas
 <?php
 }
